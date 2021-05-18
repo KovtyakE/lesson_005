397ecbe72dbb3312974de5c1a2f5764825bfcb27
Evgeniy Kovtyak <evgeniy-kovtyak@mail.ru>
2021-05-18 17:47:19 +0300

    1 task completed, TODO deleted

--- 01_inhabitants.py
+++ 01_inhabitants.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
-
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
+def printing(folks, __name__):
+    print("In the", __name__, "living next citizens:", end=' ')
+    for citizen in folks:
+        if citizen != folks[-1]:
+            print(citizen, ",", sep="", end=' ')
+        else:
+            print(citizen, end=' ')
+    print(" ")
 
-# TODO здесь ваш код
 
+from room_1 import folks, __name__
+printing(folks, __name__)
 
+from room_2 import folks, __name__
+printing(folks, __name__)
