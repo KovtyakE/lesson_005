083971a025ddf3ff658916b04c63d5d2121a19ea
EVG <evgeniy-kovtyak@mail.ru>
2021-05-20 15:26:01 +0300

    2 task with districts is completed

--- 02_district.py
+++ 02_district.py
@@ -4,9 +4,38 @@
 # Формат вывода: На районе живут ...
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
-
+new_list = []
 # TODO здесь ваш код
+def district_citizens(folks,new_list):
+    district_citizens_string = ", ".join(folks)
+    new_list += district_citizens_string + "\n"
+    return new_list
 
+from district.central_street.house1.room1 import folks
+district_citizens(folks, new_list)
 
+from district.central_street.house1.room2 import folks
+district_citizens(folks, new_list)
 
+from district.central_street.house2.room1 import folks
+district_citizens(folks, new_list)
+
+from district.central_street.house2.room2 import folks
+district_citizens(folks, new_list)
+
+from district.soviet_street.house1.room1 import folks
+district_citizens(folks, new_list)
+
+from district.soviet_street.house1.room2 import folks
+district_citizens(folks, new_list)
+
+from district.soviet_street.house2.room1 import folks
+district_citizens(folks, new_list)
+
+from district.soviet_street.house2.room2 import folks
+district_citizens_string = ", ".join(folks)
+new_list += district_citizens_string
+
+for symbol in new_list:
+    print(symbol, end="")
 
